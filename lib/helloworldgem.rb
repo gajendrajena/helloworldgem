6d75a14efe429a19e916bc0db858a190a1bd004b
Gajendra <gaju.mca@gmail.com>
2011-10-07 15:57:46 +0530

    initial commit

---
+++ lib/helloworldgem.rb
@@ -0,0 +1,10 @@
+require "helloworldgem/version"
+
+module Helloworldgem
+  def self.sayHelloWorld
+    "Helloworld !!! from gem : class method"
+  end
+  def instanceHelloWorld
+    "Helloworld !!! from gem :instance method"
+  end
+end
